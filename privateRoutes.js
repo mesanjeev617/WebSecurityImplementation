abf2876371fb1dfc08bef44a6f64236cba2db841
Sanjeev Bharati <58900304+mesanjeev617@users.noreply.github.com>
2022-02-20 10:42:39 -0700

    Update privateRoutes.js

--- privateRoutes.js
+++ privateRoutes.js
@@ -4,7 +4,7 @@ const jwt = require('jsonwebtoken');
 //we dont want to give user access without JWT....
 module.exports = function(req, res, next) {
     const token = req.header('auth-token');
-    if(!token) return res.status(401).send('Access Denied !!!! MFFF');
+    if(!token) return res.status(401).send('Access Denied !!!');
 
     try{
         const verified = jwt.verify(token, process.env.TOKEN_SECRET);
